f8f25920627409230169991148b633fa5b0358e7
Ced_M <mouneac@gmail.com>
2021-01-29 19:48:15 -0600

    Week 4 commit

---
+++ SumAverageRunningInt.java
@@ -0,0 +1,17 @@
+package warpeep.training.Week4;
+
+public class SumAverageRunningInt {
+	
+	public static void main(String[] args) {
+	
+	int sum = 0;
+	int i = 1;
+	{
+	while (i <= 100) { 
+		sum = sum + i;
+		i++;
+	}
+	System.out.print(sum);
+}
+}
+}
